bbb84d3da2362de6b0d4ccda7390fbec97ae8f7a
Rudy Ceroli <39409203+Rudy2612@users.noreply.github.com>
2022-07-29 15:56:11 +0000

    information added

---
+++ website/src/footer/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./footer.css"
+
+export default function footer() {
+    return (
+        <footer>
+            <span>Not affiliated with Hoyoverse</span>
+            <span><a href="https://twitter.com/dyrudyru">@dyrudyru</a></span>
+        </footer>
+    )
+}
