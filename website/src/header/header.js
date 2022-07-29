7109ea7df8f9048942c9e451d98d9f815d79a427
Rudy Ceroli <39409203+Rudy2612@users.noreply.github.com>
2022-07-29 15:43:56 +0000

    logo header

--- website/src/header/header.js
+++ website/src/header/header.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 
+import logo from "../data/logo.png"
+
 import './header.css'
 
 export default class header extends Component {
   render() {
     return (
       <header>
-        <p className='header-title'>Quizz Genshin</p>
+        <img className='header-logo' src={logo} alt="Genshin Impact Quizz"></img>
         <p className='header-etape'>{this.props.etape}/{this.props.total}</p>
       </header>
     )
