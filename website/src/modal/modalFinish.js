b77c84aa3976f84e623ce2f36e4e375a6bed20d4
Rudy Ceroli <39409203+Rudy2612@users.noreply.github.com>
2022-07-29 16:17:40 +0000

    modal update

--- website/src/modal/modalFinish.js
+++ website/src/modal/modalFinish.js
@@ -7,8 +7,14 @@ export default class modalFinish extends Component {
       <>
         <div className='modal-bg'></div>
         <div className='modal-body'>
-          <h3 className='modal-title'>Quizz terminé !</h3>
+
+          <h3 className='modal-title'>Vous avez terminé le quizz !</h3>
+
           <p className='modal-time'>{new Date(Date.now() - this.props.timestamp).getMinutes()}:{String(new Date(Date.now() - this.props.timestamp).getSeconds()).length === 1 ? "0" + new Date(Date.now() - this.props.timestamp).getSeconds() : new Date(Date.now() - this.props.timestamp).getSeconds()}</p>
+
+          <div className='modal-btn-twitter'>
+            <a target="_blank" rel="noreferrer" href={`https://twitter.com/intent/tweet?text=J%27ai%20trouv%C3%A9%20tous%20les%20personnages%20de%20Genshin%20Impact%20en%20${new Date(Date.now() - this.props.timestamp).getMinutes()} minutes%20!`}>Partager sur Twitter</a>
+          </div>
         </div>
       </>
     )
